7753bba35164fe32b81c3093e08558b6429453ba
Phil Tsaryk <phil@tsaryk.com>
2019-07-31 15:53:40 +0200

    rules: Turn off @typescript-eslint/indent for prettier

--- addons/prettier.js
+++ addons/prettier.js
@@ -7,6 +7,7 @@ module.exports = {
       extends: ['plugin:prettier/recommended'],
 
       rules: {
+        '@typescript-eslint/indent': 'off',
         '@typescript-eslint/member-delimiter-style': 'off',
       },
     },
