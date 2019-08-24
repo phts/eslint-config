f580de231d1071e20c7d972e54cd97bbd9b03696
Phil Tsaryk <phil@tsaryk.com>
2019-08-24 12:51:54 +0200

    rules: Refactor typescript rules to use recommended

--- rules/typescript/index.js
+++ rules/typescript/index.js
@@ -3,32 +3,24 @@
 const basic = require('../basic')
 
 module.exports = {
-  parser: '@typescript-eslint/parser',
+  extends: 'plugin:@typescript-eslint/recommended',
   parserOptions: {
     project: './tsconfig.json',
-    sourceType: 'module',
     tsconfigRootDir: '.',
   },
   env: {
     es6: true,
   },
-  plugins: ['@typescript-eslint'],
   rules: {
-    camelcase: 'off',
-    indent: 'off',
+    'no-array-constructor': 'error',
     'no-extra-parens': 'off',
-    'no-unused-vars': 'off',
-
     'import/named': 'off',
 
-    '@typescript-eslint/adjacent-overload-signatures': 'error',
     '@typescript-eslint/array-type': ['error', 'array-simple'],
     '@typescript-eslint/ban-types': 'off',
     '@typescript-eslint/camelcase': basic.rules.camelcase,
-    '@typescript-eslint/class-name-casing': 'error',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-member-accessibility': 'off',
-    '@typescript-eslint/generic-type-naming': 'off',
     '@typescript-eslint/indent': basic.rules.indent,
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/member-delimiter-style': [
@@ -43,18 +35,13 @@ module.exports = {
         },
       },
     ],
-    '@typescript-eslint/member-naming': 'off',
     '@typescript-eslint/member-ordering': 'error',
     '@typescript-eslint/no-angle-bracket-type-assertion': 'off',
     '@typescript-eslint/no-array-constructor': 'off',
     '@typescript-eslint/no-empty-interface': 'off',
     '@typescript-eslint/no-explicit-any': 'error',
     '@typescript-eslint/no-extraneous-class': 'error',
-    '@typescript-eslint/no-inferrable-types': 'error',
-    '@typescript-eslint/no-misused-new': 'error',
-    '@typescript-eslint/no-namespace': 'error',
     '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/no-object-literal-type-assertion': 'error',
     '@typescript-eslint/no-parameter-properties': 'off',
     '@typescript-eslint/no-this-alias': [
       'error',
@@ -63,14 +50,9 @@ module.exports = {
       },
     ],
     '@typescript-eslint/no-triple-slash-reference': 'error',
-    '@typescript-eslint/no-type-alias': 'off',
-    '@typescript-eslint/no-unnecessary-type-assertion': 'off',
     '@typescript-eslint/no-unused-vars': 'error',
-    '@typescript-eslint/no-use-before-define': 'error',
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/prefer-interface': 'error',
-    '@typescript-eslint/prefer-namespace-keyword': 'error',
     '@typescript-eslint/restrict-plus-operands': 'error',
-    '@typescript-eslint/type-annotation-spacing': 'error',
   },
 }
